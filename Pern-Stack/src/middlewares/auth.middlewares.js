061de5b3018f1c5354488760822df5cb7bdf3ec5
Tatiana Tamara Gimenez Rios <tatagimeri07@gmail.com>
2023-10-06 15:49:20 -0300

    JS STACKPERN-Ejecutar Middleware#204

--- Pern-Stack/src/middlewares/auth.middlewares.js
+++ Pern-Stack/src/middlewares/auth.middlewares.js
@@ -1,6 +1,22 @@
+import jwt from "jsonwebtoken";
 
 export const isAuth = (req, res, next) => {
-    console.log(req.cookies);
+    const token = req.cookies.token;
 
-    next();
-}
+    if (!token) {
+        return res.status(401).json({
+            message: 'No estas autorizado'
+        });
+    }
+
+    jwt.verify(token, "xyz123", (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                message: 'No estas autorizado'
+                    });
+        }
+        req.userId = decoded.id; //req.usuarioId luego req.userId
+        next();
+    }); 
+};
+
