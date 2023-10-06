71fa39ce2e4f8d3cfe4664b5b3af49a96a378e48
Juan Valladares <juan_1@outlook.es>
2023-10-06 09:25:38 -0300

    Test Ticket #202 y fixes generales
    Hacer una petición GET (Video 8) y fixes generales

--- Pern-Stack/src/index.js
+++ Pern-Stack/src/index.js
@@ -1,12 +1,5 @@
 import app from "./app.js";
-import {pool} from "./db.js"
 
-pool.query("SELECT NOW()", (err,res) => {
-    console.log(err,res);
-    app.listen(3000);
-    console.log('server on port',3000)
-    pool.end();
-});
+app.listen(3000);
 
-
-
+console.log("server on port", 3000);
--- Pern-Stack/src/libs/jwt.js
+++ Pern-Stack/src/libs/jwt.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
-export const createAccsessToken = (paylod) => {
+export const createAccessToken = (paylod) => {
     return new Promise((resolve, reject) => {
-        jwt.sign(paylod, "xzy123", { expiresIn: "Id" },
+        jwt.sign(paylod, "xyz123", { expiresIn: "1d" },
         (err, token) => {
             if (err) reject(err);
             resolve(token);
